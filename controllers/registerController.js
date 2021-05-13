e83ebf71a654d3d47eab44d15ab426605771936a
ABojo <alex.bojorquez2598@gmail.com>
2021-05-12 23:10:38 -0400

    Created a basic register route(I really need to improve my method of error handling so its easier to prompt the user on what exactly went wrong during registration) and I added a popup message object that can be passed into templates to give the user feedback

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,3 +1,28 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
 exports.getRegisterPage = (req, res) => {
   res.render('registerForm');
+};
+
+exports.register = async (req, res) => {
+  const { name, username, password, passwordConfirm } = req.body;
+
+  if (password === passwordConfirm) {
+    const hashedPass = await bcrypt.hash(password, 12);
+    await User.create({ name, username, password: hashedPass });
+    res.render('index', {
+      popUpMessage: {
+        status: 'success',
+        text: 'You have successfully created a new account!',
+      },
+    });
+  } else {
+    res.render('registerForm', {
+      popUpMessage: {
+        status: 'failure',
+        text: 'The passwords you have provided did not match!',
+      },
+    });
+  }
 };
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const registerController = require('../controllers/registerController');
 
 router.get('/', registerController.getRegisterPage);
+router.post('/', registerController.register);
 
 module.exports = router;
