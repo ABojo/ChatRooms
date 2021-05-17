0a5ea3969301c1dd4689731595119b7bd3594ea3
ABojo <alex.bojorquez2598@gmail.com>
2021-05-16 22:01:35 -0400

    Made protect auth controller function redirect to login page for a better ux + made styling updates to chat rooms

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 exports.protect = (req, res, next) => {
   if (!req.user) {
     req.flash('error', 'Sorry, you must be logged in to do that!');
-    res.redirect('/');
+    res.redirect('/login');
   } else {
     next();
   }
