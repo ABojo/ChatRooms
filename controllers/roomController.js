00c597ad830e8889126470dd0d7dc4e2b64ac07d
ABojo <alex.bojorquez2598@gmail.com>
2021-05-16 20:32:42 -0400

    Added a formatted timestamp virtual property to message schema/changed sender field to just use the user's username as thats all I need when rendering the messages

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -3,7 +3,7 @@ const Message = require('../models/Message');
 
 exports.getRoomPage = async (req, res) => {
   const { name } = req.params;
-  const room = await Room.findOne({ name });
+  const room = await Room.findOne({ name }).populate('messages');
 
   res.render('room', { room });
 };
@@ -11,4 +11,20 @@ exports.getRoomPage = async (req, res) => {
 exports.sendMessage = async (req, res) => {
   const { name } = req.params;
   const room = await Room.findOne({ name });
+
+  try {
+    const message = await Message.create({
+      text: req.body.text,
+      sender: req.user.username,
+    });
+
+    room.messages.push(message._id);
+    await room.save();
+    req.flash('success', 'Message sent!');
+  } catch (err) {
+    console.log(err);
+    req.flash('error', 'Failed to send your message!');
+  }
+
+  res.redirect(room.url);
 };
--- models/Message.js
+++ models/Message.js
@@ -1,12 +1,29 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-  title: { type: String, required: [true, 'You must specify a title!'] },
   timestamp: {
     type: Date,
     required: [true, 'You must specift a timestamp!'],
-    default: Date.now(),
+    default: Date.now,
   },
   text: { type: String, required: [true, 'You must specify text!'] },
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  sender: { type: String, required: true },
 });
+
+messageSchema.virtual('formattedStamp').get(function () {
+  const stamp = this.timestamp;
+  const rawHours = stamp.getHours();
+  const rawMin = stamp.getMinutes().toString();
+
+  const timeOfDay = rawHours >= 12 ? 'PM' : 'AM';
+  const hours = rawHours > 12 ? rawHours - 12 : rawHours;
+  const minutes = rawMin.length === 1 ? `0${rawMin}` : rawMin;
+
+  return `${
+    stamp.getMonth() + 1
+  }/${stamp.getDate()} ${hours}:${minutes} ${timeOfDay}`;
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
