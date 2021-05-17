bf0f78fee07abaa5cee8bd105315c987a35a4c7c
ABojo <alex.bojorquez2598@gmail.com>
2021-05-16 20:59:37 -0400

    Added a protectroom function to only allow authorized users to access private chat rooms

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,6 +1,17 @@
 const Room = require('../models/Room');
 const Message = require('../models/Message');
 
+exports.protectRoom = async (req, res, next) => {
+  const { name } = req.params;
+  const room = await Room.findOne({ name });
+
+  if (!room.private) return next();
+  if (room.users.includes(req.user._id)) return next();
+
+  req.flash('error', 'Sorry, are not a member of that room!');
+  res.render('joinRoom');
+};
+
 exports.getRoomPage = async (req, res) => {
   const { name } = req.params;
   const room = await Room.findOne({ name }).populate('messages');
--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const roomController = require('../controllers/roomController');
 
-router.get('/:name', roomController.getRoomPage);
+router.get('/:name', roomController.protectRoom, roomController.getRoomPage);
 router.post('/:name/send', roomController.sendMessage);
 
 module.exports = router;
